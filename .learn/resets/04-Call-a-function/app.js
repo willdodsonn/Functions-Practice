9385ae142f317ea67d41f5288793ea2dee1d6007
willdodsonn <sodlax38@gmail.com>
2021-12-26 21:04:47 +0000

    eighth commit

--- .learn/resets/04-Call-a-function/app.js
+++ .learn/resets/04-Call-a-function/app.js
@@ -3,10 +3,7 @@ function calculateArea(length,edge)
 	return length * edge;
 }
 // Your code Below this line:
-function calculateArea(4,4) {
-    return 4 * 4;
-}
-console.log(calculateArea);
+
 
 
 
